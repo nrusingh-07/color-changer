9dd0f9e199ba1dbcc894cf38933c8e5481cb232a
Nrusingh prasad swain <nrusinghprasadswain@gmail.com>
2025-08-28 13:59:49 +0530

    Add initial HTML, CSS, and JavaScript files for color scheme switcher

---
+++ project.js
@@ -0,0 +1,23 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector("body");
+buttons.forEach(function (button) {
+console.log(button)
+button.addEventListener('mouseover', function (e) {
+console.log(e);
+console.log(e.target);
+if (e.target.id === 'orange'){
+  body.style.background = e.target.id;
+}
+if (e.target.id === 'white'){
+  body.style.background = e.target.id;
+}
+if (e.target.id === 'green'){
+  body.style.background = e.target.id;
+}
+if (e.target.id === 'blue'){
+  body.style.background = e.target.id;
+}
+
+})
+
+})
